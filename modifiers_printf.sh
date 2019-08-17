dcb10d440886e124374ef7f94bca128c592a8fae
sudheer <sudheerlikeu@gmail.com>
2019-08-17 09:03:28 +0000

    print modifiers

---
+++ modifiers_printf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+printf "%40s\n" "welcome to linux automation"
+echo  "
+
+
+
+
+
+
+
+
+
+
+
+"
+#var_1=50
+#printf "%*s%\n" "$var_1" "welcome to linux world of automation"
+printf "%-54s%30s\n" "welcome to linux" "world of automation"
+
---
+++ prit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+<< lol
+printf "%s\n" "hi sudheer welcome to shell automation"
+printf "%s\n" "hi sudheer,how are you man"
+
+
+
+printf "%b" "welcome to shell scripting\n"
+~
+lol
+printf "%s" "shell scripting automation\n	"
+
+a=3
+b=4
+c=$(($a+$b))
+printf "%s\n" "$a"
+printf "%d\n" "$b"
+printf "%d\n" "$c"
